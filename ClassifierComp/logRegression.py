2e89148516ccd2ff43b14861a8570f22b5b531e2
Sameer Khanna <sameer_khanna@berkeley.edu>
2019-08-28 09:06:40 -0700

    Update logReg and RandFor

--- ClassifierComp/logRegression.py
+++ ClassifierComp/logRegression.py
@@ -14,19 +14,27 @@ from sklearn.preprocessing import StandardScaler
 # Machine Learning
 from sklearn.linear_model import LogisticRegression
 
+# Load the csv dataset
 dataset = pd.read_csv('heart.csv')
 
+# Create dummy columns for the data with set values (EX: values only in range 1-4)
 dataset = pd.get_dummies(dataset, columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal'])
-#scale each column to appropriate values
+
+# Scale each column to appropriate values using StandardScaler
 standardScaler = StandardScaler()
 columns_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
 dataset[columns_to_scale] = standardScaler.fit_transform(dataset[columns_to_scale])
 
+# Target values are the values we are trying to predict
 y = dataset['target']
 X = dataset.drop(['target'], axis = 1)
+
+# Set aside 20% of the dataset for testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 65)
+
+# Print train/test accuracy
 print("Logistic Regression")
 lr = LogisticRegression()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 65)
 lr.fit(X_train,y_train)
 print("Train accuracy {}".format(lr.score(X_train, y_train)))
 print("Test accuracy {}".format(lr.score(X_test, y_test)))
--- ClassifierComp/randomForest.py
+++ ClassifierComp/randomForest.py
@@ -14,18 +14,25 @@ from sklearn.preprocessing import StandardScaler
 # Machine Learning
 from sklearn.ensemble import RandomForestClassifier
 
+# Load the csv dataset
 dataset = pd.read_csv('heart.csv')
 
+# Create dummy columns for the data with set values (EX: values only in range 1-4)
 dataset = pd.get_dummies(dataset, columns = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal'])
-#scale each column to appropriate values
+
+# Scale each column to appropriate values using StandardScaler
 standardScaler = StandardScaler()
 columns_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
 dataset[columns_to_scale] = standardScaler.fit_transform(dataset[columns_to_scale])
 
+# Target values are the values we are trying to predict
 y = dataset['target']
 X = dataset.drop(['target'], axis = 1)
+
+# Set aside 20% of the dataset for testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 65)
 
+# Fit and score the RF classifier on varying number of estimators
 rf_scores = []
 estimators = [10, 100, 200, 500, 1000]
 for i in estimators:
@@ -33,6 +40,7 @@ for i in estimators:
     rf_classifier.fit(X_train, y_train)
     rf_scores.append(rf_classifier.score(X_test, y_test))
 
+# Plot the data for easy analysis
 colors = rainbow(np.linspace(0, 1, len(estimators)))
 plt.bar([i for i in range(len(estimators))], rf_scores, color = colors, width = 0.8)
 plt.xticks(ticks = [i for i in range(len(estimators))], labels = [str(estimator) for estimator in estimators])
